87c559dada5810fdb0472ebdcf40c272edbae883
Muhammad Abdusy Syukur <aci@hotmail.co.id>
2019-05-29 21:05:59 +0700

    Add Socket connection

--- app/socket/index.js
+++ app/socket/index.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 4000;
 var init = function(app){
     var server 	= require('http').Server(app);
     var io = require('socket.io')(server);
+    io.on('connection', (socket) => {
+        console.log('Client connected');
+    });
 
     server.listen(PORT, () => {
         console.log(`Listening on http://localhost:${ PORT }`);
