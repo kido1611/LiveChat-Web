6b039877e2cca4cc5b22321ce3cad57e9a0838b8
Muhammad Abdusy Syukur <aci@hotmail.co.id>
2019-05-29 21:08:53 +0700

    Socket client, send message example when click send

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,8 @@
 var socket = io();
+
+$(document).ready(function(){
+    $("#form-chat").submit(function(e){
+        e.preventDefault();
+        socket.emit('newMessage', "Nama", "Pesan22");
+    });
+});
