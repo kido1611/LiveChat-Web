898e76d768b46ce7fdc8cad3a9faaae50e13bcf5
Muhammad Abdusy Syukur <aci@hotmail.co.id>
2019-05-29 21:33:50 +0700

    Socket client, add receiver for updateclient

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,9 @@ var socket = io();
 socket.on('addMessage', function(user, message){
     console.log(user+" "+message);
 });
+socket.on('updateClient', function(count){
+    console.log("Client count: "+count);
+});
 
 $(document).ready(function(){
     $("#form-chat").submit(function(e){
