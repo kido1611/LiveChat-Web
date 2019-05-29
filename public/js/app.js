47371d9f780ea9681ede42d0880af265980a3d56
Muhammad Abdusy Syukur <aci@hotmail.co.id>
2019-05-29 21:09:14 +0700

    Socket client, add connection to receive new message

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
 var socket = io();
+socket.on('addMessage', function(user, message){
+    console.log(user+" "+message);
+});
 
 $(document).ready(function(){
     $("#form-chat").submit(function(e){
