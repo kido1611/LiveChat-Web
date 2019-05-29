419fa7ef424c1f697b006ad9e0942c6611344cc3
Muhammad Abdusy Syukur <aci@hotmail.co.id>
2019-05-30 06:30:00 +0700

    JS Client, Load name from cookie

--- public/js/app.js
+++ public/js/app.js
@@ -16,3 +16,26 @@ $(document).ready(function(){
         document.cookie = "nama="+$("#nama").val();
     });
 });
+
+var nama = getCookie("nama");
+if(nama==undefined){
+    nama = "Guest"
+}
+$("#nama").val(nama);
+
+
+function getCookie(cname){
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(";");
+    for(var i = 0; i < ca.length; i++){
+        var c = ca[i];
+        while(c.charAt(0) == ' '){
+            c = c.substring(1);
+        }
+        if(c.indexOf(name) == 0){
+            return c.substring(name.length, c.length);
+        }
+    }
+    return undefined;
+}
