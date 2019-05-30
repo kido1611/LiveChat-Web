9439653623dbde013f653cfd78f6def54069ed00
Muhammad Abdusy Syukur <aci@hotmail.co.id>
2019-05-30 10:27:50 +0700

    Router, Generate UUID for user

--- app/router/index.js
+++ app/router/index.js
@@ -10,6 +10,9 @@ var init = function(socket){
     router.get('/clients', function(req,res,next){
         res.send(socket.sockets.sockets.length);
     });
+    router.get('/randomids', function(req, res, next){
+        res.send(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+    });
     router.get('/setting', function(req, res, next){
         res.render('setting');
     });
